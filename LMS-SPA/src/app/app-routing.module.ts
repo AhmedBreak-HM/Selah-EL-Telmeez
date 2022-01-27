eb31a2bfc3a1e6b18f2ca8c24e6a388da1e98c9a
ahmed break <ahmed_break8@yahoo.com>
2022-01-27 18:20:09 +0200

    Lazy module && shared  module && Auth module

--- LMS-SPA/src/app/app-routing.module.ts
+++ LMS-SPA/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./layouts/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'auth', loadChildren: () => import('./layouts/auth/auth.module').then(m => m.AuthModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
